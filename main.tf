055cc2264c4b7c067737743922bad8a6fd480619
Roger Berlind <roger@hashicorp.com>
2019-08-27 15:36:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "vsphere" {
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
 resource "vsphere_virtual_disk" "myDisk" {
   size         = 1
   vmdk_path    = "Roger/Roger.vmdk"
